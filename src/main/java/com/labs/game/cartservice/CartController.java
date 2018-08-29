a2719165574ce9e1c3a0498e362e4b7b03067283
es81446 <es81446@MacBook-Pro-de-es81446.local>
2018-08-29 16:04:38 +0200

    Donegit add src/test/resources/contracts/cart-service-777.groovy

--- src/main/java/com/labs/game/cartservice/CartController.java
+++ src/main/java/com/labs/game/cartservice/CartController.java
@@ -3,6 +3,7 @@ package com.labs.game.cartservice;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class CartController {
--- src/main/java/com/labs/game/cartservice/CartService.java
+++ src/main/java/com/labs/game/cartservice/CartService.java
@@ -1,12 +1,26 @@
 package com.labs.game.cartservice;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class CartService {
 
+    @Autowired
+    private CartRepository repository;
+
     public CartItem[] findByCustomer(String customer) {
-        return null;
+        final CartItem[] cartItems = repository.findByCustomer(customer);
+        if (cartItems == null)
+            return new CartItem[0];
+        else
+            return cartItems;
     }
 }
 
